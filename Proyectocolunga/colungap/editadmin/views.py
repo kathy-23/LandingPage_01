dcb003e225a5e5950a4e5d84da095609ef30bb9b
Thomoop <thomas.gutierrezr@gmail.com>
2021-05-22 21:06:39 -0400

    Update views.py

--- Proyectocolunga/colungap/editadmin/views.py
+++ Proyectocolunga/colungap/editadmin/views.py
@@ -2,16 +2,46 @@ from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django.shortcuts import redirect, render, HttpResponse
 from .forms import AddUserForm, RemoveUserForm, EditUserForm
+'''
+la forma de agregar y usuarios cambio ya que si se remueve a un usuario de la bdd y luego se vuelve agregar, su id cambiaria
+y si anteriormente ese usuario eliminado hizo alguna publicacion en el foro iban a existir conflictos por id
+quizas mostraba que cierto comentario lo hizo x persona cuando en realidad no fue asi
+asi que la eliminacion de usuario ahora en realidad no lo elimina de la bdd sino que deja al usuario inactivo 
+en la linea 115 y 69 se puede ver esto ya que los datos que obtengo para mostrar en la lista poseen una excepcion de si el usuario esta activo o no
+'''
+
 
 def addUser(request,redir=''):
+    #obtiene por POST la informacion del formulario
     adduserform = AddUserForm(request.POST)
-    if request.method == 'POST':    
-        if adduserform.is_valid():
-            adduserform.save()
+    #consulto si el metodo es POST 
+    if request.method == 'POST':
+        #almaceno el email del formulario
+        email=request.POST.get('email')
+        #creo un objeto usuario con el email que recibi del formulario
+        usuario=User.objects.get(email=email)
+        #pregunto si el objeto creado contiene datos
+        if usuario is not None:
+            #pregunto si el llenaod de formulario es valido (todos los campos llenados y password con mayusculas y minusculas y minimo 8 caracteres)
+            #el correo registrado ya existe en la base de datos, preguntar si desea sobreescribir o usar el existente?
+            # opcion sobreescribir
+            '''if adduserform.is_valid():
+                    adduserform.save()
+                    return redirect('addUser')'''
+            #usar existente
+            usuario.is_active=True
+            usuario.save()
             return redirect('addUser')
+        #si el usuario no existe
         else:
-            return HttpResponse('<h1>no se pudo agregar el usuario</h1>')
-
+            #valido el formulario
+            if adduserform.is_valid():
+                #guardo los datos del formulario
+                adduserform.save()
+                return redirect('addUser')
+            else:
+                return HttpResponse('<h1>no se pudo agregar el usuario</h1>')
+        
     if redir=='main.html':
         return redirect('main')
     elif redir=='addUser.html':
@@ -36,7 +66,7 @@ def addUser(request,redir=''):
 
 def removeUser(request,redir=''):
     #multiples obejetos con la inf de los usuarios
-    usuarios=User.objects.all()
+    usuarios=User.objects.exclude(is_active=False).all()
     #return HttpResponse('<h1>'+str(usuarios)+'</h1>')
     #cantidad de usuarios a mostrar en la lista
     cant_user="size="+str(len(usuarios))
@@ -48,7 +78,8 @@ def removeUser(request,redir=''):
         #crea el objeto de la tabla que contiene ese email
         remov = User.objects.get(email=email)
         if remov is not None:#si encontro al usuario 'not none'
-            remov.delete()#borrar en la tabla a lo que corresponde el objeto
+            remov.is_active=False #borrar en la tabla a lo que corresponde el objeto
+            remov.save()
             #return HttpResponse('<h1>'+str(remov)+'</h1>')
             return redirect('removeUser')#redirecciona a la misma pagina
         else:
@@ -81,7 +112,7 @@ def removeUser(request,redir=''):
 
 def editUser(request,redir=''):
 
-    usuarios=User.objects.all()
+    usuarios=User.objects.exclude(is_active=False).all()
     cant_user="size="+str(len(usuarios))
     if request.method == 'POST':
         save=str(request.POST.get('save'))
@@ -111,7 +142,7 @@ def editUser(request,redir=''):
             #return HttpResponse('<h1>'+str(usuario.first_name)+usuario.last_name+usuario.email+'</h1>')
             if password=='':
                 usuario.save()
-                usuarios=User.objects.all()
+                usuarios=User.objects.exclude(is_active=False).all()
                 return render(request,'editUser.html',{
                     'showform':'False',
                     'save':'False',
@@ -121,7 +152,7 @@ def editUser(request,redir=''):
             else:
                 usuario.set_password(request.POST.get('password'))
                 usuario.save()
-                usuarios=User.objects.all()
+                usuarios=User.objects.exclude(is_active=False).all()
                 return render(request,'editUser.html',{
                     'showform':'False',
                     'save':'False',
