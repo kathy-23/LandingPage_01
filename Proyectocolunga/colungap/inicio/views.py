53c0398b7427ef98f458d2f8872a4b6834e92e6e
Thomoop <thomas.gutierrezr@gmail.com>
2021-05-31 00:35:28 -0400

    frontend inicio-principal
    avance front inicio-principal
    
    Co-Authored-By: katherin5928 <62607313+katherin5928@users.noreply.github.com>
    Co-Authored-By: kathy-23 <62617863+kathy-23@users.noreply.github.com>
    Co-Authored-By: Maussi02 <37519496+Maussi02@users.noreply.github.com>

--- Proyectocolunga/colungap/inicio/views.py
+++ Proyectocolunga/colungap/inicio/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect,HttpResponse
 
 from django.contrib.auth.models import User, AbstractUser 
-
+from anuncios.models import Anuncio
 
 
 def inicio(request,redir=""):
     usuario =User.objects.get(id=request.user.id)
     username=usuario.first_name+" "+usuario.last_name
+    anuncios = Anuncio.objects.all().order_by('-fecha')
     #return HttpResponse('<h1>'+str(request.user.id)+'</h1>')
     if redir=='main.html':
         return redirect('main')
@@ -21,6 +22,7 @@ def inicio(request,redir=""):
     elif redir=='inicio-topicos.html':
         return redirect('inicio_topicos')
     return render(request, 'inicio.html',{
+        'Anuncios':anuncios,
         'username':username,
         'userid': request.user.id,
     })
